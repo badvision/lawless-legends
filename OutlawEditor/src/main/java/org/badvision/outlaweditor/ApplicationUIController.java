cf441192fcfcf1ef18fe4050cb20f8c9693121dc
Brendan Robert <brendan.robert@gmail.com>
2014-05-11 02:08:49 -0500

    Moved Map Editor UI into its own FXML and controller class -- tracking down some regressions but it is mostly still working OK.

--- OutlawEditor/src/main/java/org/badvision/outlaweditor/ApplicationUIController.java
+++ OutlawEditor/src/main/java/org/badvision/outlaweditor/ApplicationUIController.java
@@ -5,33 +5,33 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.ListView;
 import javafx.scene.control.Menu;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import org.badvision.outlaweditor.data.xml.Image;
-import org.badvision.outlaweditor.data.xml.Map;
-import org.badvision.outlaweditor.data.xml.Script;
-import org.badvision.outlaweditor.data.xml.Tile;
 
 public abstract class ApplicationUIController {
+
     public static ApplicationUIController getController() {
         return Application.instance.controller;
     }
 
-    abstract void rebuildMapSelectors();
     abstract void rebuildTileSelectors();
+
     abstract void rebuildImageSelector();
 
-    abstract Editor getVisibleEditor();    
-    
+    abstract Editor getVisibleEditor();
+
     @FXML // ResourceBundle that was given to the FXMLLoader
     protected ResourceBundle resources;
-    @FXML ApplicationMenuController menuController;
-    @FXML TileEditorTabController tileEditorController;
-    
+    @FXML
+    ApplicationMenuController menuController;
+    @FXML
+    TileEditorTabController tileController;
+    @FXML
+    MapEditorTabController mapController;
+
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     protected URL location;
     @FXML // fx:id="imageCategoryField"
@@ -48,24 +48,8 @@ public abstract class ApplicationUIController {
     protected ComboBox<Image> imageSelector; // Value injected by FXMLLoader
     @FXML // fx:id="imageWidthField"
     protected TextField imageWidthField; // Value injected by FXMLLoader
-    @FXML // fx:id="mapEditorAnchorPane"
-    protected AnchorPane mapEditorAnchorPane; // Value injected by FXMLLoader
-    @FXML // fx:id="mapHeightField"
-    protected TextField mapHeightField; // Value injected by FXMLLoader
-    @FXML // fx:id="mapNameField"
-    protected TextField mapNameField; // Value injected by FXMLLoader
-    @FXML // fx:id="mapScriptsList"
-    protected ListView<Script> mapScriptsList; // Value injected by FXMLLoader
-    @FXML // fx:id="mapSelect"
-    protected ComboBox<Map> mapSelect; // Value injected by FXMLLoader
-    @FXML
-    protected Menu mapSelectTile;
-    @FXML // fx:id="mapWidthField"
-    protected TextField mapWidthField; // Value injected by FXMLLoader
-    @FXML // fx:id="mapWrapAround"
-    protected CheckBox mapWrapAround; // Value injected by FXMLLoader
-
     // Handler for MenuItem[javafx.scene.control.MenuItem@3a4bc91a] onAction
+
     @FXML
     abstract public void imageBitMode(ActionEvent event);
 
@@ -85,9 +69,6 @@ public abstract class ApplicationUIController {
     @FXML
     abstract public void imageShift(ActionEvent event);
 
-    @FXML
-    abstract public void imageTabActivated(Event event);
-    
     // Handler for MenuItem[javafx.scene.control.MenuItem@547638c0] onAction
     @FXML
     abstract public void imageTogglePanZoom(ActionEvent event);
@@ -99,32 +80,6 @@ public abstract class ApplicationUIController {
     // Handler for Button[Button[id=null, styleClass=button zoomOutButton]] onAction
     @FXML
     abstract public void imageZoomOut(ActionEvent event);
-
-    @FXML
-    abstract public void mapDraw1(ActionEvent event);
-
-    @FXML
-    abstract public void mapDraw3(ActionEvent event);
-
-    @FXML
-    abstract public void mapDraw5(ActionEvent event);
-
-    @FXML
-    abstract public void mapDrawFilledRectMode(ActionEvent event);
-
-    @FXML
-    abstract public void mapTabActivated(Event event);
-
-    @FXML
-    abstract public void mapTogglePanZoom(ActionEvent event);
-
-    // Handler for Button[Button[id=null, styleClass=button zoomInButton]] onAction
-    @FXML
-    abstract public void mapZoomIn(ActionEvent event);
-
-    // Handler for Button[Button[id=null, styleClass=button zoomOutButton]] onAction
-    @FXML
-    abstract public void mapZoomOut(ActionEvent event);
 
     // Handler for Button[Button[id=null, styleClass=button]] onAction
     @FXML
@@ -146,41 +101,6 @@ public abstract class ApplicationUIController {
     @FXML
     abstract public void onImageSelected(ActionEvent event);
 
-    // Handler for Button[Button[id=null, styleClass=button]] onAction
-    @FXML
-    abstract public void onMapClonePressed(ActionEvent event);
-
-    // Handler for Button[Button[id=null, styleClass=button]] onAction
-    @FXML
-    abstract public void onMapCreatePressed(ActionEvent event);
-
-    // Handler for Button[Button[id=null, styleClass=button]] onAction
-    @FXML
-    abstract public void onMapDeletePressed(ActionEvent event);
-
-    // Handler for Button[Button[id=null, styleClass=button]] onAction
-    @FXML
-    abstract public void onMapExportPressed(ActionEvent event);
-
-    @FXML
-    abstract public void onMapPreviewPressed(ActionEvent event);
-
-    // Handler for Button[Button[id=null, styleClass=button]] onAction
-    @FXML
-    abstract public void onMapScriptAddPressed(ActionEvent event);
-
-    // Handler for Button[Button[id=null, styleClass=button]] onAction
-    @FXML
-    abstract public void onMapScriptClonePressed(ActionEvent event);
-    
-    // Handler for Button[Button[id=null, styleClass=button]] onAction
-    @FXML
-    abstract public void onMapScriptDeletePressed(ActionEvent event);
-
-    // Handler for ComboBox[id="tileSelect"] onAction
-    @FXML
-    abstract public void onMapSelected(ActionEvent event);
-    
     abstract public void platformChange();
 
     // Handler for Button[Button[id=null, styleClass=button moveButton]] onAction
@@ -199,27 +119,17 @@ public abstract class ApplicationUIController {
     @FXML
     abstract public void scrollImageUp(ActionEvent event);
 
-    // Handler for Button[Button[id=null, styleClass=button moveButton]] onAction
-    @FXML
-    abstract public void scrollMapDown(ActionEvent event);
-
-    // Handler for Button[Button[id=null, styleClass=button moveButton]] onAction
-    @FXML
-    abstract public void scrollMapLeft(ActionEvent event);
-
-    // Handler for Button[Button[id=null, styleClass=button moveButton]] onAction
-    @FXML
-    abstract public void scrollMapRight(ActionEvent event);
-
-    // Handler for Button[Button[id=null, styleClass=button moveButton]] onAction
-    @FXML
-    abstract public void scrollMapUp(ActionEvent event);
-
     @FXML
     abstract public void imageDraw5BitMode(ActionEvent event);
 
     @FXML
     abstract public void tileTabActivated(Event event);
+
+    @FXML
+    abstract public void mapTabActivated(Event event);
+
+    @FXML
+    abstract public void imageTabActivated(Event event);
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     public void initialize() {
@@ -230,17 +140,8 @@ public abstract class ApplicationUIController {
         assert imagePatternMenu != null : "fx:id=\"imagePatternMenu\" was not injected: check your FXML file 'ApplicationUI.fxml'.";
         assert imageSelector != null : "fx:id=\"imageSelector\" was not injected: check your FXML file 'ApplicationUI.fxml'.";
         assert imageWidthField != null : "fx:id=\"imageWidthField\" was not injected: check your FXML file 'ApplicationUI.fxml'.";
-        assert mapEditorAnchorPane != null : "fx:id=\"mapEditorAnchorPane\" was not injected: check your FXML file 'ApplicationUI.fxml'.";
-        assert mapHeightField != null : "fx:id=\"mapHeightField\" was not injected: check your FXML file 'ApplicationUI.fxml'.";
-        assert mapNameField != null : "fx:id=\"mapNameField\" was not injected: check your FXML file 'ApplicationUI.fxml'.";
-        assert mapScriptsList != null : "fx:id=\"mapScriptsList\" was not injected: check your FXML file 'ApplicationUI.fxml'.";
-        assert mapSelect != null : "fx:id=\"mapSelect\" was not injected: check your FXML file 'ApplicationUI.fxml'.";
-        assert mapSelectTile != null : "fx:id=\"mapSelectTile\" was not injected: check your FXML file 'ApplicationUI.fxml'.";
-        assert mapWidthField != null : "fx:id=\"mapWidthField\" was not injected: check your FXML file 'ApplicationUI.fxml'.";
-        assert mapWrapAround != null : "fx:id=\"mapWrapAround\" was not injected: check your FXML file 'ApplicationUI.fxml'.";
 
         // Initialize your logic here: all @FXML variables will have been injected
-
     }
 
     abstract void completeInflightOperations();
--- OutlawEditor/src/main/java/org/badvision/outlaweditor/ApplicationUIControllerImpl.java
+++ OutlawEditor/src/main/java/org/badvision/outlaweditor/ApplicationUIControllerImpl.java
@@ -6,24 +6,18 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.ComboBoxListCell;
-import javafx.scene.image.ImageView;
-import javafx.scene.image.WritableImage;
 import javafx.scene.input.DataFormat;
 import javafx.util.Callback;
 import static org.badvision.outlaweditor.Application.currentPlatform;
-import static org.badvision.outlaweditor.Application.gameData;
 import static org.badvision.outlaweditor.UIAction.*;
 import static org.badvision.outlaweditor.data.PropertyHelper.*;
 import org.badvision.outlaweditor.data.TileUtils;
 import org.badvision.outlaweditor.data.TilesetUtils;
 import org.badvision.outlaweditor.data.xml.Image;
-import org.badvision.outlaweditor.data.xml.Script;
 import org.badvision.outlaweditor.data.xml.Tile;
 
 /**
@@ -33,8 +27,6 @@ import org.badvision.outlaweditor.data.xml.Tile;
  */
 public class ApplicationUIControllerImpl extends ApplicationUIController {
 
-    public org.badvision.outlaweditor.data.xml.Map currentMap = null;
-    public MapEditor currentMapEditor = null;
     public Image currentImage = null;
     public ImageEditor currentImageEditor = null;
 
@@ -46,33 +38,6 @@ public class ApplicationUIControllerImpl extends ApplicationUIController {
             @Override
             public void observedObjectChanged(Object object) {
                 rebuildTileSelectors();
-            }
-        });
-
-        mapSelect.setButtonCell(new ComboBoxListCell<org.badvision.outlaweditor.data.xml.Map>() {
-            {
-                super.setPrefWidth(125);
-            }
-
-            @Override
-            public void updateItem(org.badvision.outlaweditor.data.xml.Map item, boolean empty) {
-                textProperty().unbind();
-                super.updateItem(item, empty);
-                if (item != null) {
-                    textProperty().bind(mapNameField.textProperty());
-                } else {
-                    setText(null);
-                }
-            }
-        });
-        mapSelect.setCellFactory(new Callback<ListView<org.badvision.outlaweditor.data.xml.Map>, ListCell<org.badvision.outlaweditor.data.xml.Map>>() {
-            @Override
-            public ListCell<org.badvision.outlaweditor.data.xml.Map> call(ListView<org.badvision.outlaweditor.data.xml.Map> param) {
-                return new EntitySelectorCell<org.badvision.outlaweditor.data.xml.Map>(mapNameField) {
-                    @Override
-                    public void finishUpdate(org.badvision.outlaweditor.data.xml.Map item) {
-                    }
-                };
             }
         });
 
@@ -168,55 +133,6 @@ public class ApplicationUIControllerImpl extends ApplicationUIController {
     }
 
     @Override
-    public void mapDraw1(ActionEvent event) {
-        if (currentMapEditor != null) {
-            currentMapEditor.setDrawMode(MapEditor.DrawMode.Pencil1px);
-        }
-    }
-
-    @Override
-    public void mapDraw3(ActionEvent event) {
-        if (currentMapEditor != null) {
-            currentMapEditor.setDrawMode(MapEditor.DrawMode.Pencil3px);
-        }
-    }
-
-    @Override
-    public void mapDraw5(ActionEvent event) {
-        if (currentMapEditor != null) {
-            currentMapEditor.setDrawMode(MapEditor.DrawMode.Pencil5px);
-        }
-    }
-
-    @Override
-    public void mapDrawFilledRectMode(ActionEvent event) {
-        if (currentMapEditor != null) {
-            currentMapEditor.setDrawMode(MapEditor.DrawMode.FilledRect);
-        }
-    }
-
-    @Override
-    public void mapTogglePanZoom(ActionEvent event) {
-        if (currentMapEditor != null) {
-            currentMapEditor.togglePanZoom();
-        }
-    }
-
-    @Override
-    public void mapZoomIn(ActionEvent event) {
-        if (currentMapEditor != null) {
-            currentMapEditor.zoomIn();
-        }
-    }
-
-    @Override
-    public void mapZoomOut(ActionEvent event) {
-        if (currentMapEditor != null) {
-            currentMapEditor.zoomOut();
-        }
-    }
-
-    @Override
     public void onImageClonePressed(ActionEvent event) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
@@ -259,80 +175,15 @@ public class ApplicationUIControllerImpl extends ApplicationUIController {
     }
 
     @Override
-    public void onMapClonePressed(ActionEvent event) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void onMapCreatePressed(ActionEvent event) {
-        org.badvision.outlaweditor.data.xml.Map m = new org.badvision.outlaweditor.data.xml.Map();
-        m.setName("Untitled");
-        gameData.getMap().add(m);
-        m.setWidth(512);
-        m.setHeight(512);
-        setCurrentMap(m);
-        rebuildMapSelectors();
-    }
-
-    @Override
-    public void onMapDeletePressed(ActionEvent event) {
-        if (currentMap == null) {
-            return;
-        }
-        confirm("Delete map '" + currentMap.getName() + "'.  Are you sure?", new Runnable() {
-            @Override
-            public void run() {
-                org.badvision.outlaweditor.data.xml.Map del = currentMap;
-                setCurrentMap(null);
-                Application.gameData.getMap().remove(del);
-                rebuildMapSelectors();
-            }
-        }, null);
-    }
-
-    @Override
-    public void onMapExportPressed(ActionEvent event) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void onMapPreviewPressed(ActionEvent event) {
-        if (currentMapEditor == null) {
-            return;
-        }
-        currentMapEditor.showPreview();
-    }
-
-    @Override
-    public void onMapScriptAddPressed(ActionEvent event) {
-        createAndEditScript();
-    }
-
-    @Override
-    public void onMapScriptClonePressed(ActionEvent event) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void onMapScriptDeletePressed(ActionEvent event) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    @Override
-    public void onMapSelected(ActionEvent event) {
-        setCurrentMap(mapSelect.getSelectionModel().getSelectedItem());
-    }
-    
-    @Override
     public void platformChange() {
         for (Tile t : Application.gameData.getTile()) {
             TileUtils.redrawTile(t);
         }
-        Tile tile = tileEditorController.getCurrentTile();
+        Tile tile = tileController.getCurrentTile();
         rebuildTileSelectors();
-        tileEditorController.setCurrentTile(tile);
-        if (currentMapEditor != null) {
-            currentMapEditor.redraw();
+        tileController.setCurrentTile(tile);
+        if (mapController.getCurrentEditor() != null) {
+            mapController.getCurrentEditor().redraw();
         }
         rebuildImageSelector();
     }
@@ -366,111 +217,9 @@ public class ApplicationUIControllerImpl extends ApplicationUIController {
     }
 
     @Override
-    public void scrollMapDown(ActionEvent event) {
-        if (currentMapEditor != null) {
-            currentMapEditor.scrollBy(0, 1);
-        }
-    }
-
-    @Override
-    public void scrollMapLeft(ActionEvent event) {
-        if (currentMapEditor != null) {
-            currentMapEditor.scrollBy(-1, 0);
-        }
-    }
-
-    @Override
-    public void scrollMapRight(ActionEvent event) {
-        if (currentMapEditor != null) {
-            currentMapEditor.scrollBy(1, 0);
-        }
-    }
-
-    @Override
-    public void scrollMapUp(ActionEvent event) {
-        if (currentMapEditor != null) {
-            currentMapEditor.scrollBy(0, -1);
-        }
-    }
-
-    @Override
     public void rebuildTileSelectors() {
-        tileEditorController.rebuildTileSelectors();
-        mapSelectTile.getItems().clear();
-        for (final Tile t : Application.gameData.getTile()) {
-            WritableImage img = TileUtils.getImage(t, currentPlatform);
-            ImageView iv = new ImageView(img);
-            MenuItem mapSelectItem = new MenuItem(t.getName(), iv);
-            mapSelectItem.setGraphic(new ImageView(TileUtils.getImage(t, currentPlatform)));
-            mapSelectItem.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-                    if (currentMapEditor != null) {
-                        currentMapEditor.setCurrentTile(t);
-                    }
-                }
-            });
-            mapSelectTile.getItems().add(mapSelectItem);
-        }
-    }
-
-    public void setCurrentMap(org.badvision.outlaweditor.data.xml.Map m) {
-        if (currentMap != null && currentMap.equals(m)) {
-            return;
-        }
-//        mapEditorAnchorPane.getChildren().clear();
-        currentMap = m;
-        if (currentMapEditor != null) {
-            currentMapEditor.unregister();
-        }
-        if (m == null) {
-            bind(mapHeightField.textProperty(), null);
-            bind(mapNameField.textProperty(), null);
-            bind(mapWidthField.textProperty(), null);
-            bind(mapWrapAround.selectedProperty(), null);
-            mapHeightField.setDisable(true);
-            mapNameField.setDisable(true);
-            mapWidthField.setDisable(true);
-            mapWrapAround.setDisable(true);
-            currentMapEditor = null;
-        } else {
-            if (m.getHeight() == null) {
-                m.setHeight(512);
-            }
-            if (m.getWidth() == null) {
-                m.setWidth(512);
-            }
-            if (m.getName() == null) {
-                m.setName("Untitled");
-            }
-            try {
-                mapHeightField.setDisable(false);
-                mapNameField.setDisable(false);
-                mapWidthField.setDisable(false);
-                mapWrapAround.setDisable(false);
-//                bind(mapHeightField.textProperty(), intProp(m, "height"));
-                bind(mapNameField.textProperty(), stringProp(m, "name"));
-//                bind(mapWidthField.textProperty(), intProp(m, "width"));
-//                bind(mapWrapAround.selectedProperty(),boolProp(m, "wrap"));
-            } catch (NoSuchMethodException ex) {
-                Logger.getLogger(ApplicationUIControllerImpl.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            currentMapEditor = new MapEditor();
-            currentMapEditor.setEntity(m);
-            currentMapEditor.buildEditorUI(mapEditorAnchorPane);
-        }
-        redrawMapScripts();
-    }
-
-    @Override
-    public void rebuildMapSelectors() {
-        mapSelect.getItems().clear();
-        mapSelect.getItems().addAll(Application.gameData.getMap());
-        mapSelect.getSelectionModel().select(getCurrentMap());
-    }
-
-    public org.badvision.outlaweditor.data.xml.Map getCurrentMap() {
-        return currentMap;
+        tileController.rebuildTileSelectors();
+        mapController.rebuildTileSelectors();
     }
 
     private void setCurrentImage(Image i) {
@@ -531,8 +280,8 @@ public class ApplicationUIControllerImpl extends ApplicationUIController {
 
     @Override
     public void completeInflightOperations() {
-        if (currentMapEditor != null) {
-            currentMapEditor.currentMap.updateBackingMap();
+        if (mapController.getCurrentEditor() != null) {
+            mapController.getCurrentEditor().currentMap.updateBackingMap();
         }
     }
 
@@ -563,50 +312,14 @@ public class ApplicationUIControllerImpl extends ApplicationUIController {
             case image:
                 return currentImageEditor;
             case map:
-                return currentMapEditor;
+                return mapController.getCurrentEditor();
             case tile:
-                return tileEditorController.getCurrentTileEditor();
+                return tileController.getCurrentTileEditor();
         }
         return null;
     }
 
     public static final DataFormat SCRIPT_DATA_FORMAT = new DataFormat("MythosScript");
-    public void redrawMapScripts() {
-        mapScriptsList.setOnEditStart(new EventHandler<ListView.EditEvent<Script>>() {
-            @Override
-            public void handle(ListView.EditEvent<Script> event) {
-                  UIAction.editScript(event.getSource().getItems().get(event.getIndex()));
-            }
-        });
-        final TransferHelper<Script> scriptDragDrop = new TransferHelper<>(Script.class);
-        mapScriptsList.setCellFactory(new Callback<ListView<Script>, ListCell<Script>>() {
-            @Override
-            public ListCell<Script> call(ListView<Script> param) {
-                final ListCell<Script> cell = new ListCell<Script>() {
-                    @Override
-                    protected void updateItem(Script item, boolean empty) {
-                        super.updateItem(item, empty);
-                        if (empty || item == null) {                            
-                            setText("");
-                        } else {
-                            setText(item.getName());
-                            scriptDragDrop.registerDragSupport(this, item);
-                        }
-                    }
-                };
-                return cell;
-            }
-        });
-        if (currentMap == null) {
-            mapScriptsList.getItems().clear();
-        } else {
-            if (mapScriptsList.getItems() != null && currentMap.getScripts() != null) {
-                mapScriptsList.getItems().setAll(currentMap.getScripts().getScript());
-            } else {
-                mapScriptsList.getItems().clear();
-            }
-        }
-    }
 
     abstract public static class EntitySelectorCell<T> extends ComboBoxListCell<T> {
 
@@ -652,4 +365,5 @@ public class ApplicationUIControllerImpl extends ApplicationUIController {
         public void finishUpdate(T item) {
         }
     };
+
 }

ebe9c8ea14cf92e9eb3bff39afafafbfe50b17bd
David Schmenk <dschmenk@gmail.com>
2017-08-09 13:19:57 -0700

    New portable VM and fix parse typo

--- Platform/Apple/tools/PLASMA/src/plvm.c
+++ Platform/Apple/tools/PLASMA/src/plvm.c
@@ -1,12 +1,3 @@
-/*
- * Copyright (C) 2015 The 8-Bit Bunch. Licensed under the Apache License, Version 1.1
- * (the "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at <http://www.apache.org/licenses/LICENSE-1.1>.
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
- * ANY KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -785,7 +776,9 @@ void interp(code *ip)
                 fp += PLA;
             case 0x5C: // RET : IP = TOFP
                 return;
-            case 0x5E: // ???
+            case 0x5E: // CFFB : TOS = CONSTANTBYTE(IP) | 0xFF00
+                PUSH(BYTE_PTR(ip) | 0xFF00);
+                ip++;
                 break;
                 /*
                  * 0x60-0x6F

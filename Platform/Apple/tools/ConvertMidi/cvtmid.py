bf5fd4fafad4b87f9964c4e16ebe867a573b60de
Dave Schmenk <dschmenk@gmail.com>
2017-11-07 16:24:09 -0800

    Better delta time tracking of MIDI events

--- Platform/Apple/tools/ConvertMidi/cvtmid.py
+++ Platform/Apple/tools/ConvertMidi/cvtmid.py
@@ -5,13 +5,16 @@ import sys
 import mido
 
 mid = mido.MidiFile(sys.argv[1])
+timescale = 16.0
 totaltime = 0
-perctime  = 0
+eventtime = 0.0
 for msg in mid:
+    eventtime += msg.time * timescale
+    #print '; time = ', msg.time
     if msg.type == 'note_on' or msg.type == 'note_off':
-        if msg.time > 0.0 and msg.time < 1.0/16.0:
-            msg.time = 1.0/16.0
-        deltatime = int(msg.time * 16)
+        if eventtime > 0.0 and eventtime < 0.5:
+            eventtime = 0.5
+        deltatime = int(eventtime + 0.5)
         octave    = int(msg.note / 12 - 1)
         onote     = int(msg.note % 12)
         lrchan    = int(msg.channel & 1)
@@ -23,16 +26,27 @@ for msg in mid:
         if octave < 0:
             octave = 0
         totaltime += deltatime
-        if msg.channel == 9:
+        if msg.channel == 9 or msg.channel == 8:
             #
             # Percussion
             #
-            if vol > 0 and (totaltime - perctime) > 4:
-                print '\t!BYTE\t${0:02X}, ${1:02X}, ${2:02X}\t; Percussion'.format(deltatime, msg.note >> 3, 2)
-                perctime = totaltime
+            if vol > 0:
+                print '\t!BYTE\t${0:02X}, ${1:02X}, ${2:02X}\t; Percussion {3:d} Chan {4:d} Dur {5:d}'.format(deltatime, msg.note ^ 0x40, (lrchan << 7) | vol, msg.note, msg.channel, vol)
+                eventtime = 0.0
         else:
             #
             # Note
             #
             print '\t!BYTE\t${0:02X}, ${1:02X}, ${2:02X}\t; Note {3:d} Chan {4:d} Vol {5:d}'.format(deltatime, 0x80 | (octave << 4) | onote, (lrchan << 7) | vol, msg.note, msg.channel, vol)
-print '\t!BYTE\t$00, $00, $00'
+            eventtime = 0.0
+    elif msg.type == 'set_tempo':
+        pass
+        timescale = msg.tempo / 500000.0 * 16.0
+        print '; timescale = ', timescale
+    elif msg.type == 'time_signature':
+        pass
+    elif msg.type == 'control_chage':
+        pass
+    elif msg.type == 'program_change':
+        pass
+print '\t!BYTE\t${0:02X}, $00, $00'.format(int(eventtime + 0.5))

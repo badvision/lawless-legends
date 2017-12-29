18c77f06e02e9d2bcf6e03884ac9a8237da67068
Brendan Robert <brobert@adobe.com>
2017-12-29 16:07:22 -0600

    Updated game packaging script

--- Platform/Apple/tools/AppPackaging/build_all.bat
+++ Platform/Apple/tools/AppPackaging/build_all.bat
@@ -1,3 +1,6 @@
-java -jar packr.jar --platform mac --jdk https://www.dropbox.com/s/rpz6z283ymrslj0/mac_jre_1.8.151.zip?dl=0 --resources ../jace/target/LawlessLegends.jar --executable LawlessLegends --classpath LawlessLegends.jar --mainclass jace.LawlessLegends --vmargs Xmx1G --output "Lawless Legends.app" --icon logo_transparent.icns --minimizejre oraclejre8
+java -jar packr.jar --platform mac --jdk https://www.dropbox.com/s/nnodj0om38bnypk/mac_jre_1.8.151.zip?dl=1 --resources ../jace/target/LawlessLegends.jar --executable LawlessLegends --classpath LawlessLegends.jar --mainclass jace.LawlessLegends --vmargs Xmx1G --output "Lawless Legends.app" --icon game_icon.icns
 
-java -jar packr.jar --platform windows64 --jdk https://www.dropbox.com/s/i4gxp4bhz97j9wx/win_jre_1.8.151.zip?dl=0 --resources ../jace/target/LawlessLegends.jar --executable LawlessLegends --classpath LawlessLegends.jar --mainclass jace.LawlessLegends --vmargs Xmx1G --output "Lawless Legends" --minimizejre oraclejre8
+java -jar packr.jar --platform windows64 --jdk https://www.dropbox.com/s/i4gxp4bhz97j9wx/win_jre_1.8.151.zip?dl=1 --resources ../jace/target/LawlessLegends.jar --executable LawlessLegends --classpath LawlessLegends.jar --mainclass jace.LawlessLegends --vmargs Xmx1G --output "Lawless Legends"
+
+copy /BY game.2mg "Lawless Legends"
+copy /BY game.2mg "Lawless Legends.app\Contents\Resources"

929d884e4157b02505eb1831713380c4a7b8ecff
Martin Haye <martin.haye@ucop.edu>
2019-05-02 09:24:19 -0700

    Better recording.

--- Platform/Apple/virtual/xformScript.rb
+++ Platform/Apple/virtual/xformScript.rb
@@ -1,18 +1,7 @@
 #!/usr/bin/env ruby
 
-length = 0
 File.readlines(ARGV[0]).each do |line|
-  line.scan(/KEY:(.*?) SEED:/).each { |match|
-    print $1
-    length += 1
-    if length > 80
-      print "\n"
-      length = 0
-    end
-  }
-  line.scan(/STRING:(.*)/).each { |match|
-    puts $1
-    length = 0
-  }
+  line.scan(/KEY:(.*?) SEED:/).each { |match| print $1 }
+  line.scan(/STRING:(.*)/).each { |match| puts $1 }
 end
 print "\n"

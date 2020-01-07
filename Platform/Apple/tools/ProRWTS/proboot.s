e6f7bdaba308d14fcdb209488e402fd5cf4ac411
Peter Ferrie <peter.ferrie@gmail.com>
2020-01-06 19:46:30 -0800

    newer ProRWTS2

--- Platform/Apple/tools/ProRWTS/proboot.s
+++ Platform/Apple/tools/ProRWTS/proboot.s
@@ -1,35 +1,34 @@
 ;license:BSD-3-Clause
 ;minimal open/read binary file in ProDOS filesystem
-;copyright (c) Peter Ferrie 2016-2017
+;copyright (c) Peter Ferrie 2016-2018
 !cpu 6502
+!to "proboot",plain
 *=$800
 
                 ;zpage usage, arbitrary selection except for the "PROM constant" ones
-                tmpsec    = $3c
+                tmpval    = $3c
                 reqsec    = $3d         ;PROM constant
-                A1L       = $3c
-                A1H       = $3d
-                curtrk    = $40         ;PROM constant
-                adrlo     = $44
-                adrhi     = $45
+                A2L       = $3e
+                A2H       = $3f
+                phase     = $40
+                reqtrk    = $41         ;PROM constant
+                adrlo     = $26
+                adrhi     = $27
 
                 sizehi    = $53
-                entries   = $f8         ;(internal) total number of entries in directory
                 step      = $fd         ;(internal) state for stepper motor
-                tmptrk    = $fe         ;(internal) temporary copy of current track
-                phase     = $ff         ;(internal) current phase for seek
 
                 ;constants
                 scrn2p2   = $f87b
-                dirbuf    = $e00        ;for size-optimisation
+                dirbuf    = $1e00       ;for size-optimisation
                 PHASEOFF  = $c080
                 Q6L       = $c08c
 
                 !byte 1
                 lsr
                 bne init
-                inc reqsec
-                txa
+goprom2         inc reqsec
+goprom1         txa
                 jsr scrn2p2
                 ora #$c0
                 pha
@@ -45,136 +44,156 @@ fakeMLI         pla
                 txa
                 pha
                 rts
+fakeMLI_e
 
 filename        !byte filename_e - filename_b
-filename_b      !text "LEGENDOS.SYSTEM" ;your start-up file, file is min 513 bytes, max 64kb
+filename_b      !text "LEGENDOS.SYSTEM" ;your start-up file, file is max 40kb
 filename_e
 
-init            txa
-                tay
-                ora #$80
-                sta unrseek+1
-                ora #$0c
-                sta unrread1+1
-                sta unrread2+1
-                sta unrread3+1
-                sta unrread4+1
-
--               lda fakeMLI-1, x
-                sta $beff, x
-                lda #$ff
-                sta $bf0f, x
-                dex
+init
+-               txa
+                jsr scrn2p2
+                ora #$c0
+                sta $be30, y
+                lda fakeMLI_e-$100, y
+                sta $be00+fakeMLI_e-fakeMLI, y
+                iny
                 bne -
-                sty $bf30
-
-                sta $200
+                stx $bf30
+                sty $200
+                sta $801
 
 opendir         ;read volume directory key block
-                txa
                 ldx #2
-                jsr readdirsel
 
                 ;include volume directory header in count
 
-firstent        lda #<(dirbuf+4)
-                sta A1L
-                lda #>(dirbuf+4)
-                sta A1H
+firstent        lda #>dirbuf
+                sta adrhi
+                sta A2H
+                jsr seekread
+                lda #4
+                sta A2L
 nextent         ldy #0
-                lda (A1L), y
 
                 ;match name lengths before attempting to match names
 
-                lda (A1L), y
+                lda (A2L), y
                 and #$0f
                 tax
                 inx
-                !byte $2c
--               lda (A1L), y
-                cmp filename, y
+-               cmp filename, y
                 beq foundname
 
                 ;move to next directory in this block
 
-+               clc
-                lda A1L
+                clc
+                lda A2L
                 adc #$27
-                sta A1L
+                sta A2L
                 bcc +
 
                 ;there can be only one page crossed, so we can increment instead of adc
 
-                inc A1H
-+               cmp #<(dirbuf+$1ff) ;4+($27*$0d)
-                lda A1H
-                sbc #>(dirbuf+$1ff)
-                bcc nextent
+                inc A2H
++               cmp #$ff ;4+($27*$0d)
+                bne nextent
 
                 ;read next directory block when we reach the end of this block
 
                 ldx dirbuf+2
-                lda dirbuf+3
-                jsr readdirsec
-                bne firstent
+                ldy dirbuf+3
+                bcs firstent
 
 foundname       iny
+                lda (A2L), y
                 dex
                 bne -
-
-                ;cache EOF (file size)
-
-                ldy #$15
-                lda (A1L), y
-                cmp #1
-                iny
-                lda (A1L), y
-                adc #1
-                lsr
-                sta sizehi
+                stx $ff
 
                 ;cache KEY_POINTER
 
                 ldy #$11
-                lda (A1L), y
+                lda (A2L), y
                 tax
                 iny
-                lda (A1L), y
+                lda (A2L), y
+                tay
 
-                ;read index block in case of sapling
+                lda #>dirbuf
+                sta adrhi
 
-                jsr readdirsec
+readfile        jsr seekread
 
-                ;restore load offset
-
-                asl adrhi
-
-readfile
                 ;fetch data block and read it
 
-                ldy $41 ;zeroed by boot PROM
-                inc $41
+blockind        ldy $ff
+                inc $ff
                 ldx dirbuf, y
                 lda dirbuf+256, y
-                jsr seekread
-
-                ;loop while size-$200 is non-zero
-
-                dec sizehi
+                tay
+                bne readfile
+                txa
                 bne readfile
 
 readdone        jmp $2000
 
-                ;no tricks here, just the regular stuff
+step1           !byte 1, $30, $28, $24, $20, $1e, $1d, $1c
+step2           !byte $70, $2c, $26, $22, $1f, $1e, $1d, $1c
 
-seek            sty step
-                asl phase
+readadr
+-               jsr readd5aa
+                eor #$96
+                bne -
+                sta step
+                jsr +
++               jsr readnib
+                rol
+                sta tmpval
+                bcs readnib
+
+readd5aa        ldx $2b
+--              jsr readnib
+-               cmp #$d5
+                bne --
+                jsr readnib
+                cmp #$aa
+                bne -
+
+readnib
+-               lda Q6L, x
+                bpl -
+seekret         rts
+
+                ;convert block number to track/sector
+
+seekread        tya
+                lsr
                 txa
+                ror
+                lsr
+                lsr
+                sta reqtrk
+                txa
+                and #3
+                php
+                asl
+                plp
+                rol
+                sta reqsec
+                jsr readadr
+                and tmpval
+
+                ;if track does not match, then seek
+
+                cmp reqtrk
+                beq checksec
+                asl reqtrk
                 asl
 copy_cur        tax
-                sta tmptrk
+                sta phase
                 sec
-                sbc phase
+                sbc reqtrk
                 beq +++
                 bcs +
                 eor #$ff
@@ -195,148 +214,31 @@ copy_cur        tax
 +++             php
                 bne +
 ---             clc
-                lda tmptrk
+                lda phase
                 ldx step2, y
-+               stx tmpsec
++               stx tmpval
                 and #3
                 rol
+                ora $2b
                 tax
                 lsr
-unrseek
                 lda PHASEOFF, x
---              ldx #$13
+--              ldx #$12
 -               dex
-                bne -
-                dec tmpsec
+                bpl -
+                dec tmpval
                 bne --
                 bcs ---
                 plp
-                beq seekret
+                beq shifttrk
                 pla
                 inc step
                 bne copy_cur
 
-step1           !byte 1, $30, $28, $24, $20, $1e, $1d, $1c
-step2           !byte $70, $2c, $26, $22, $1f, $1e, $1d, $1c
+shifttrk        lsr reqtrk
 
-readadr
--               jsr readd5aa
-                cmp #$96
-                bne -
-                ldy #3
--               sta curtrk
-                jsr readnib
-                rol
-                sta tmpsec
-                jsr readnib
-                and tmpsec
-                dey
-                bne -
-seekret         rts
+checksec        ldx $2b
+                jsr goprom1
+                jmp goprom2
 
-readd5aa
---              jsr readnib
--               cmp #$d5
-                bne --
-                jsr readnib
-                cmp #$aa
-                bne -
-                tay                    ;we need Y=#$AA later
-
-readnib
-unrread1
--               lda Q6L
-                bpl -
-                rts
-
-readdirsel      sta adrlo
-readdirsec
-                ldy #>dirbuf
-                sty adrhi
-
-                ;convert block number to track/sector
-
-seekread        lsr
-                txa
-                ror
-                lsr
-                lsr
-                sta phase
-                txa
-                and #3
-                php
-                asl
-                plp
-                rol
-                sta reqsec
-                jsr readadr
-
-                ;if track does not match, then seek
-
-                ldx curtrk
-                cpx phase
-                beq checksec
-                jsr seek
-
-                ;force sector mismatch
-
-                lda #$ff
-
-                ;match or read sector
-
-checksec        jsr cmpsec
-                inc reqsec
-                inc reqsec
-
-                ;force sector mismatch
-
-cmpsecrd        lda #$ff
-
-cmpsec          cmp reqsec
-                beq readdata
-                jsr readadr
-                beq cmpsec
-
-                ;read sector data
-
-readdata        jsr readd5aa
-                eor #$ad                ;zero A if match
-;;                bne *                   ;lock if read failure
-unrread2
--               ldx Q6L
-                bpl -
-                eor nibtbl - $96, x
-                sta bit2tbl - $aa, y
-                iny
-                bne -
-unrread3
--               ldx Q6L
-                bpl -
-                eor nibtbl-$96, x
-                sta (adrlo), y          ;the real address
-                iny
-                bne -
-unrread4
--               ldx Q6L
-                bpl -
-                eor nibtbl-$96, x
-                bne cmpsecrd
---              ldx #$a9
--               inx
-                beq --
-                lda (adrlo), y
-                lsr bit2tbl - $aa, x
-                rol
-                lsr bit2tbl - $aa, x
-                rol
-                sta (adrlo), y
-                iny
-                bne -
-readret         inc adrhi
-                rts
-
-bit2tbl         = $300 ;PROM constant
-nibtbl          = bit2tbl + 108 ;PROM constant
-dataend         = nibtbl + 106 ;PROM constant
-readbuff
 !byte $D3,$C1,$CE,$A0,$C9,$CE,$C3,$AE

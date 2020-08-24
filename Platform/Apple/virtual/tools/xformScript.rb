52837c1a350b2e877e2e83c555275e940326239b
Martin Haye <martin.haye@ucop.edu>
2020-08-24 09:56:50 -0700

    Switched record mode key to just 'R' to save time.

--- Platform/Apple/virtual/tools/xformScript.rb
+++ Platform/Apple/virtual/tools/xformScript.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+print "R"
 File.readlines(ARGV[0]).each do |line|
   line.scan(/KEY:(.*?) SEED:/).each { |match| print $1 }
   line.scan(/STRING:(.*)/).each { |match| puts $1 }

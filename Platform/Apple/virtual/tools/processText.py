8d9f6fe627cfa23e1ca17383f355e11ccc8403ca
Martin Haye <martin.haye@ucop.edu>
2021-09-02 09:21:18 -0700

    processText now can re-integrate changes

--- Platform/Apple/virtual/tools/processText.py
+++ Platform/Apple/virtual/tools/processText.py
@@ -8,38 +8,52 @@ import xml.etree.ElementTree as ET
 
 seenhashes = set()
 scriptlines = 0
+changing = False
+new_hashes = {}
 
+###############################################################################
 def hashstr(s):
     md5 = hashlib.md5(s.encode())
     return re.sub(r'=+$', '', base64.b32encode(md5.digest()[0:6]).decode())
 
+###############################################################################
 def processsentence(sentence):
     global scriptlines
     h = hashstr(sentence)
-    if h in seenhashes:
-        print(f"{h}    <dupe>")
+    if changing:
+        assert h in new_hashes, f"hmm, hash {h!r} is missing from new_hashes"
+        return new_hashes[h]
     else:
-        print(f"{h}    {sentence}")
+        if h in seenhashes:
+            print(f"{h}\t<dupe>")
+        else:
+            print(f"{h}\t{sentence}")
+        scriptlines += 1
         seenhashes.add(h)
-    scriptlines += 1
 
+###############################################################################
 def processnode(textfield):
     text = textfield.text
     if text is not None:
         lines = re.split(r'(\^[mM])', text)
+        out = []
         for i in range(0, len(lines), 2):
             line = lines[i]
             sep = lines[i+1] if i+1 < len(lines) else None
             assert line is not None
-            #if sep is not None:
-            #    processsentence(sep)
             parts = re.split(r'((?<!Mr)(?<!Mrs)(?<!Ms)(?<!Dr)[.!?…]+[)”"=]?\s*)', line)
-            #print(f"parts={parts!r}")
             for j in range(0, len(parts), 2):
                 sentence = parts[j] + (parts[j+1] if j+1 < len(parts) else '')
                 if not sentence == "":
-                    processsentence(sentence)
+                    out.append(processsentence(sentence))
+                    # TODO: deal with whitespace at end of sentence
+            if changing and sep:
+                out.append(sep)
+        if changing:
+            newtext = "".join(out)
+            textfield.text = newtext
 
+###############################################################################
 def trav(node, parentpath):
     global scriptlines
     if node.tag.endswith("script"):
@@ -55,9 +69,41 @@ def trav(node, parentpath):
     for kid in node:
         trav(kid, path)
 
+###############################################################################
+def read_hashes(filename):
+    out = {}
+    with open(filename, "r") as io:
+        for line in io.readlines():
+            if line == '\n':
+                continue
+            line = line.replace("\ufeff", "") # Get rid of byte order mark from Word
+            m = re.match(r'^(?P<hashcode>[A-Z0-9]{10})\t(?P<sentence>.*)\n$', line)
+            assert m, f"Can't parse {line!r}"
+            d = m.groupdict()
+            if m['sentence'] != "<dupe>":
+                out[m['hashcode']] = m['sentence']
+    return out
+
+###############################################################################
+# From https://stackoverflow.com/questions/54439309/how-to-preserve-namespaces-when-parsing-xml-via-elementtree-in-python
+def register_all_namespaces(filename):
+    namespaces = dict([node for _, node in ET.iterparse(filename, events=['start-ns'])])
+    for ns in namespaces:
+        ET.register_namespace(ns, namespaces[ns])
+
+###############################################################################
+
+register_all_namespaces(sys.argv[1])
 tree = ET.parse(sys.argv[1])
+if len(sys.argv) == 3:  # progname infile newhashes
+    new_hashes = read_hashes(sys.argv[2])
+    changing = True
+else:
+    changing = False
+
 trav(tree.getroot(), [])
 
-#for el in tree.iter():
-#    if 'type' in el.attrib and 'text_print' in el.attrib['type']
-#    print(f"tag={el.tag} type={el.attrib['type'] if 'type' in el.attrib else None} text={el.text!r}")
+if changing:
+    with open('out.xml', 'wb') as io:
+        io.write("""<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n""".encode())
+        tree.write(io, encoding='utf-8')

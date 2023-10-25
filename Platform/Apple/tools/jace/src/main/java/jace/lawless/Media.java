06c16733c2803bcdc6c5c54ae38142e9857cbac9
Brendan Robert <brobert@adobe.com>
2023-10-24 22:16:26 -0500

    Filling in needed methods for playback

--- Platform/Apple/tools/jace/src/main/java/jace/lawless/Media.java
+++ Platform/Apple/tools/jace/src/main/java/jace/lawless/Media.java
@@ -1,8 +1,32 @@
 package jace.lawless;
 
+import javafx.util.Duration;
+
 public class Media {
 
     public Media(String resourcePath) {
+    }
+
+    public void seekToTime(Duration millis) {
+    }
+
+    public boolean isEnded() {
+        return false;
+    }
+
+    public void restart() {
+    }
+
+    public short getNextLeftSample() {
+        return 0;
+    }
+
+    public short getNextRightSample() {
+        return 0;
+    }
+
+    public java.time.Duration getCurrentTime() {
+        return null;
     }
     
 }
--- Platform/Apple/tools/jace/src/main/java/jace/lawless/MediaPlayer.java
+++ Platform/Apple/tools/jace/src/main/java/jace/lawless/MediaPlayer.java
@@ -1,6 +1,12 @@
 package jace.lawless;
 
 import java.time.Duration;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import jace.core.SoundMixer;
+import jace.core.SoundMixer.SoundBuffer;
 
 public class MediaPlayer {
 
@@ -8,6 +14,9 @@ public class MediaPlayer {
     int repeats = 0;
     int maxRepetitions = 1;
     Status status = Status.STOPPED;
+    Media song;
+    SoundBuffer playbackBuffer;
+    Executor executor = Executors.newSingleThreadExecutor();
 
     public static enum Status {
         PLAYING, PAUSED, STOPPED
@@ -16,14 +25,15 @@ public class MediaPlayer {
     public static final int INDEFINITE = -1;
 
     public MediaPlayer(Media song) {
+        this.song = song;
     }
 
     public Status getStatus() {
-        return null;
+        return status;
     }
 
     public Duration getCurrentTime() {
-        return null;
+        return song.getCurrentTime();
     }
 
     public double getVolume() {
@@ -32,6 +42,11 @@ public class MediaPlayer {
 
     public void stop() {
         status = Status.STOPPED;
+        try {
+            playbackBuffer.shutdown();
+        } catch (InterruptedException | ExecutionException e) {
+            // Ignore exception on shutdown
+        }
     }
 
     public void setCycleCount(int i) {
@@ -43,13 +58,40 @@ public class MediaPlayer {
     }
 
     public void setStartTime(javafx.util.Duration millis) {
+        song.seekToTime(millis);
     }
 
     public void play() {
         repeats = 0;
         status = Status.PLAYING;
+        if (playbackBuffer == null || !playbackBuffer.isAlive()) {
+            playbackBuffer = SoundMixer.createBuffer(true);
+        }
+        executor.execute(() -> {
+            while (status == Status.PLAYING && (maxRepetitions == INDEFINITE || repeats < maxRepetitions)) {
+                if (song.isEnded()) {
+                    if (maxRepetitions == INDEFINITE) {
+                        song.restart();
+                    } else {
+                        repeats++;
+                        if (repeats < maxRepetitions) {
+                            song.restart();
+                        } else {
+                            this.stop();
+                            break;
+                        }
+                    }
+                }
+                short leftSample = song.getNextLeftSample();
+                short rightSample = song.getNextRightSample();
+                try {
+                    playbackBuffer.playSample((short) (leftSample * vol));
+                    playbackBuffer.playSample((short) (rightSample * vol));
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                    this.stop();
+                }
+            }
+        });
     }
-
-
-
 }

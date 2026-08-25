610fcea58cd30405b2ff77fab1cd8b7bd6a82379
Martin Haye <martin.haye@ucop.edu>
2026-08-25 11:42:58 -0700

    Allow for keys without seed in xformScript.rb

--- Platform/Apple/virtual/tools/xformScript.rb
+++ Platform/Apple/virtual/tools/xformScript.rb
@@ -2,7 +2,7 @@
 
 print "R"
 File.readlines(ARGV[0]).each do |line|
-  line.scan(/KEY:(.*?) SEED:/).each { |match| print $1 }
+  line.scan(/KEY:(.*)/).each { |match| print $1 }
   line.scan(/STRING:(.*)/).each { |match| puts $1 }
 end
 print "\n"
